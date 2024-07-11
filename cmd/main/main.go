c7d89c7dbfe81649736b1183d36baec997b0e2bc
canitey <mohamedmed01345@gmail.com>
2024-07-11 12:02:34 +0300

    made main.go smaller

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,61 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
+	"tooly/internal/ui"
 )
 
-type model struct {
-	choices []string
-	cursor int
-	selected map[int]bool
-}
-
-func initialModel(toolList []string) model {
-	return model{
-		choices: toolList,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg :
-		switch msg.String() {
-		case "k":
-			if !(m.cursor <= 0) {
-				m.cursor--
-			}
-		case "j":
-			if m.cursor < len(m.choices) - 1 {
-				m.cursor++
-			}
-		case "q":
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) View() string {
-	s := "tool list\n\n"
-
-	for i, toolName := range m.choices {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
-		s += fmt.Sprintf("%s %s\n",cursor, toolName)
-	}
-
-	return s
-}
 
 
 
@@ -67,10 +15,7 @@ func main() {
 		"nmap",
 		"naabu",
 	}
-	model := initialModel(toolist)
-    p := tea.NewProgram(model)
-    if _, err := p.Run(); err != nil {
-        fmt.Printf("Alas, there's been an error: %v", err)
-        os.Exit(1)
-    }
+
+	model := ui.InitialModel(toolist)
+	ui.Run(model)
 }
